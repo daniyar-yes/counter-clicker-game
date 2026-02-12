6b57904837ef0c9f21f730c57a9dc217b65bbae8
daniyar-yes <yeskaliyevdan@hotmail.com>
2026-02-11 19:05:29 -0800

    add first ever JS, and some CSS for future development

---
+++ main.js
@@ -0,0 +1,9 @@
+var firstVariable = 10000;
+
+let secondVariable = 20;
+
+const thirdValiable = 50;
+
+let result = firstVariable + secondVariable + thirdValiable;
+
+console.log('sum of ', 'first variable:', firstVariable, 'second and third variable:', secondVariable, thirdValiable, 'equals to: ', result)
