442f9cac72daa062663709ff4549b6746110cc94
daniyar-yes <yeskaliyevdan@hotmail.com>
2026-02-23 18:27:42 -0800

    hooked the query selectors to the html elements on screen

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+const plusOneButton = document.querySelector("#plus-one-btn");
+const mainCounterRow = document.querySelector(".main-counter-row");
+const matches = document.querySelectorAll("h4");
+
+console.log(plusOneButton)
+console.log(mainCounterRow)
+console.log(matches)
